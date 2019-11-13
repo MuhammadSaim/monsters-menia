b12cc766b7fbaee675d2499c65b0bcea96542f86
muhammadsaim <muhammadsaim494@gmail.com>
2019-11-13 21:59:12 +0500

    Updates

---
+++ precache-manifest.ba5a0cc0f62ea706c42d0daaaad11c1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f6c3597238e3160f47f00b9a55ba91",
+    "url": "/MuhammadSaim/monsters-menia/index.html"
+  },
+  {
+    "revision": "374b617c4ae094c06a6d",
+    "url": "/MuhammadSaim/monsters-menia/static/css/main.46929505.chunk.css"
+  },
+  {
+    "revision": "c6214f979b742b0bcadf",
+    "url": "/MuhammadSaim/monsters-menia/static/js/2.f65d83a6.chunk.js"
+  },
+  {
+    "revision": "374b617c4ae094c06a6d",
+    "url": "/MuhammadSaim/monsters-menia/static/js/main.68da5bae.chunk.js"
+  },
+  {
+    "revision": "0c8c7cd483264ef978e6",
+    "url": "/MuhammadSaim/monsters-menia/static/js/runtime-main.8022fa14.js"
+  }
+]);
